2f898e9b2e4ca404478b35d4fde0b7ac31b2f8c0
Timofey Shichalin <76839122+timshicha@users.noreply.github.com>
2023-03-27 19:52:53 -0700

    removed unnecessary code

--- config.js
+++ config.js
@@ -4,7 +4,3 @@ const path = require("path");
 dotenv.config({
     path: path.resolve(__dirname, "./env")
 });
-
-module.exports = {
-    REACT_APP_API_BASE_URL: process.env.REACT_APP_API_BASE_URL
-}

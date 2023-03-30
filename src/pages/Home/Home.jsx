359e57aad1dff75e2b44176534fc829369cbc36b
Timofey Shichalin <76839122+timshicha@users.noreply.github.com>
2023-03-29 21:37:21 -0700

    some initial login functionality

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,48 @@
 import React, { useEffect, useState } from "react";
 import { Link, Navigate } from "react-router-dom";
-// import { socket } from "../socket";
+import { socket } from "../socket";
 
 function Home() {
 
+    const sendMessage = () => {
+        socket.emit("message", "ms");
+    }
+
+    const login = () => {
+        let username = document.getElementById("login-username").value;
+        let password = document.getElementById("login-password").value;
+
+        fetch(process.env.REACT_APP_API_BASE_URL + "/api/sessions/create", {
+            method: "POST",
+            body: JSON.stringify({ "username": username, "password": password }),
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+        }).then(res => res.json()).then(res => {
+            // If successful
+            if (res.success) {
+                console.log("Login successful.");
+                console.log(res);
+            }
+            else {
+                console.log("Could not log in.");
+            }
+        });
+    }
+
     return (
         <>
+            Username:<input type="text" id="login-username"></input><br />
+            Password:<input type="password" id="login-password"></input><br />
+            <button onClick={login}>Login</button>
+
+
             <p>Home</p>
             <Link to="/signup">Login</Link><br />
             <Link to="/dot-game">Dot Game</Link>
+
+            <button onClick={sendMessage}>Send message</button>
         </>
     );
 }
